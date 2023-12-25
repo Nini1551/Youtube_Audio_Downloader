fc98efb45b474edaac0c4d8720dac19432f0b46f
Nini1551 <47613768+Nini1551@users.noreply.github.com>
2023-12-25 19:43:07 +0100

    add the download of multiples url links at the same time

--- README.md
+++ README.md
@@ -5,9 +5,12 @@ by HUYBRECHTS Louis
 ## Mode d'emploi
 Télécharge l'audio d'une vidéo Youtube au format mp4.
 L'utilisateur doit passer en paramètres certains arguments à l'appel du script :
-- url : L'url de la vidéo Youtube à télécharger.
-- output : Le chemin du dossier où sera téléchargée la vidéo.
+- **url** : L'url de la vidéo Youtube à télécharger.
+  Par défaut, une surprise vous attend.
+- **output** : Le chemin du dossier où sera téléchargée la vidéo.
   Si le dossier n'existe pas, il est créé.
   Par défaut, le fichier sera téléchargé dans un dossier download_arguments situé dans le dossier courant.
-- progress : Si la progression du téléchargement doit être affichée ou non.
+- **progress** : Si la progression du téléchargement doit être affichée ou non.
   Par défaut, la progression ne sera pas affichée.
+- **file** : fichier texte composé d'urls de vidéo Youtube à télécharger séparées par des retours à la ligne.
+  L'option n'est pas obligatoire.
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 """
 YOUTUBE AUDIO DOWNLOADER
 by HUYBRECHTS Louis
-23/12/2023
 
 Télécharge l'audio d'une vidéo Youtube au format mp4.
 L'utilisateur doit passer en paramètres certains arguments à l'appel du script :
 - url : L'url de la vidéo Youtube à télécharger.
+  Par défaut, une surprise vous attend.
 - output : Le chemin du dossier où sera téléchargée la vidéo.
   Si le dossier n'existe pas, il est créé.
   Par défaut, le fichier sera téléchargé dans un dossier download_arguments situé dans le dossier courant.
 - progress : Si la progression du téléchargement doit être affichée ou non.
   Par défaut, la progression ne sera pas affichée.
+- -f (--file) : fichier texte composé d'urls de vidéo Youtube à télécharger séparées par des retours à la ligne.
+  L'option n'est pas obligatoire.
 """
 import os
+import time
 import argparse
 from pytube import YouTube
 
@@ -36,15 +39,19 @@ def download_audio(url: str, output_path: str = '.', display_progress: bool = Fa
     """
     Télécharge l'audio d'une vidéo Youtube au format mp4
     PRE : - url: url d'une vidéo Youtube publique et non-limitée aux utilisateurs majeurs
-          - output_path: chemin du dossier dans lequel le fichier mp4 sera téléchargée
-            Par défaut, les fichiers seront téléchargées dans le dossier courant.
+          - output_path: chemin du dossier dans lequel le fichier mp4 sera téléchargé
+            Par défaut, le fichier sera téléchargé dans le dossier courant.
           - display_progress: un booléen
             Par défaut, faux
     POST : Télécharge l'audio d'une vidéo Youtube au format mp4 via son url.
            Le fichier sera téléchargé dans le dossier précisé par l'utilisateur.
            Précise le début et la fin du téléchargement en plus du nom de la vidéo téléchargée.
            Si l'option est sélectionnée, affiche la progression du téléchargement.
+           Si l'url est une chaîne vide, une surprise vous attend.
     """
+    if url == '':
+        url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'
+
     on_progress_callback = on_progress if display_progress else None
 
     yt = YouTube(url, on_progress_callback=on_progress_callback)
@@ -52,36 +59,69 @@ def download_audio(url: str, output_path: str = '.', display_progress: bool = Fa
 
     print(f"Téléchargement de la vidéo : \"{yt.title}\"")
     audio.download(output_path)
-    print("Téléchargement terminé.")
+    print("Téléchargement terminé.\n")
+
+def download_audios_from_file(filename: str, output_path: str = '.', display_progress: bool = False):
+    """
+
+    PRE : - filename : nom d'un fichier texte composé d'urls de vidéos Youtube séparées par un retour à la ligne
+          - output_path: chemin du dossier dans lequel les fichiers mp4 seront téléchargés
+            Par défaut, les fichiers seront téléchargés dans le dossier courant.
+          - display_progress: un booléen
+            Par défaut, faux
+    POST : Télécharge l'audio de plusieurs vidéos Youtube au format mp4 via le nom du fichier texte.
+           Les fichiers seront téléchargés dans le dossier précisé par l'utilisateur.
+           Précise le début et la fin du téléchargement en plus du nom de la vidéo téléchargée.
+           Entre chaque téléchargement, un temps d'attente est posé.
+           Ignore les lignes vides.
+           Si l'option est sélectionnée, affiche la progression du téléchargement.
+    """
+    sleep_time = 1
+    with open(filename, 'r') as file:
+        for url in file:
+            url = url.strip()
+            if url != '':
+                download_audio(url, output_path, display_progress)
+                time.sleep(sleep_time)
 
 def get_args() -> argparse.Namespace:
     """
     Crée et fournit un utilitaire des arguments du script lancé
     PRE : Les arguments suivants doivent être précisés au lancement du script.
             - L'url d'une vidéo Youtube publique et non-limitée aux utilisateurs majeurs
+              Par défaut, une chaîne vide (surprise ...)
             - -o (--output) : chemin du dossier dans lequel le fichier mp4 sera téléchargée
               Par défaut, les fichiers seront téléchargées dans le dossier courant.
             - --progress : stocke vrai
+            - -f (--file) : nom d'un fichier texte ou None (pas obligatoire).
+              Le fichier texte est composé d'urls de vidéos Youtube séparées par un retour à la ligne.
+              Par défaut, None
     POST : Renvoie l'utilitaire des arguments du cript lancé aux propriétés suivantes:
            - url (str) : url d'une vidéo Youtube publique et non-limitée aux utilisateurs majeurs
            - output_path (str) : chemin du dossier dans lequel le fichier mp4 sera téléchargée
            - progress (bool) : si la progression du téléchargement doit être affichée ou non
+           - file (str) : fichier texte composé d'urls de vidéo Youtube à télécharger séparées par des retours à la ligne
     """
     parser = argparse.ArgumentParser(description="Télécharge l'audio d'une vidéo YouTube au format audio mp4.")
     parser.add_argument(
         'url',
-        type=str,
+        type=str, default='', nargs='?',
         help="URL de la vidéo YouTube"
     )
     parser.add_argument(
         '-o', '--output',
-        type=str, default='./download_audios',
+        type=str, default='./downloaded_audios',
         help="Chemin de sortie (par défaut: dossier courant)"
     )
     parser.add_argument(
         '--progress',
         action='store_true',
         help="Si la progression du téléchargement doit être affichée ou non"
+    )
+    parser.add_argument(
+        '-f', '--file',
+        type=str, default=None,
+        help="Fichier texte composé d'urls de vidéo Youtube à télécharger séparées par des retours à la ligne"
     )
     return parser.parse_args()
 
@@ -91,4 +131,7 @@ if __name__ == '__main__':
 
     os.makedirs(args.output, exist_ok=True)
 
-    download_audio(args.url, args.output, args.progress)
+    if args.file is None:
+        download_audio(args.url, args.output, args.progress)
+    else:
+        download_audios_from_file(args.file, args.output, args.progress)
