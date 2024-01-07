0e6219fd949a29cd25c650951134010c30947a0b
Nini1551 <47613768+Nini1551@users.noreply.github.com>
2024-01-07 17:16:35 +0100

    add open_browser function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import webbrowser
 from lib.youtube_downloader import download_audio
 from flask import Flask, render_template, redirect, request, url_for
 
@@ -13,6 +14,15 @@ def download():
     url = request.form.get('url')
     download_audio(url)
     return redirect('/')
+
+def open_browser():
+    """
+    Ouvre le navigateur par défaut de l'utilisateur sur la page de l'application Youtube Audio Downloader
+    PRE : -
+    POST : Ouvre le navigateur par défaut de l'utilisateur sur la page la première adresse locale.
+           Si l'application est lancée, la page équivaut à celle de l'application Youtube Audio Downloader
+    """
+    webbrowser.open_new('http://127.0.0.1:5000/')
 
 
 if __name__ == '__main__':
